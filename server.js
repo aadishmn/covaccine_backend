81beb4fd095ed5202285eb8dee33c5bbcd4b704e
aadishmn <83083983+aadishmn@users.noreply.github.com>
2023-05-08 20:53:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,17 +13,12 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Headers", "*")
-    res.header(
-        "Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE"
-    )
-    res.header(
-        "Access-Control-Allow-Headers", "Content-Type, x-requested-with"
-
-    )
-    next();
-})
+		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Credentials", "true");
+		res.setHeader("Access-Control-Max-Age", "1800");
+		res.setHeader("Access-Control-Allow-Headers", "content-type");
+		res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+		// res.setHeader("Content-Type", "application/json;charset=utf-8"); // Opening this comment will cause problems
 app.use(express.json());
 app.use(moragan("dev"));
 
